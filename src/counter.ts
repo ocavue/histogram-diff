406458908cbdf389be60056febd405e45ea9cc53
ocavue <ocavue@gmail.com>
2026-01-24 04:34:00 +1100

    chore: format code

--- src/counter.ts
+++ src/counter.ts
@@ -1,13 +1,12 @@
-
 export class Counter<K> extends Map<K, number> {
-    override get(key: K): number {
-        return super.get(key) ?? 0
-    }
+  override get(key: K): number {
+    return super.get(key) ?? 0
+  }
 
-    /**
-     * Increments the count for a key by a given amount (default 1).
-     */
-    increment(key: K, amount = 1): void {
-        this.set(key, this.get(key) + amount)
-    }
+  /**
+   * Increments the count for a key by a given amount (default 1).
+   */
+  increment(key: K, amount = 1): void {
+    this.set(key, this.get(key) + amount)
+  }
 }
